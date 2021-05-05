c518ed46e4cad70d457fc8b4696a5ad2cf1da989
changnam go <changnma.go@gmail.com>
2021-05-05 16:24:50 +0900

    add servlet listener and spring applicationlistener

--- src/main/java/com/honsoft/web/SpringbootwebApplication.java
+++ src/main/java/com/honsoft/web/SpringbootwebApplication.java
@@ -1,11 +1,23 @@
 package com.honsoft.web;
 
+import javax.servlet.ServletContextListener;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import com.honsoft.web.filter.MyFilter;
+import com.honsoft.web.listener.AppContextListener;
+import com.honsoft.web.servlet.MyServlet;
 
 @SpringBootApplication
+@ServletComponentScan 
 public class SpringbootwebApplication {
 	
 	private static ApplicationContext applicationContext;
@@ -14,7 +26,7 @@ public class SpringbootwebApplication {
 		applicationContext = SpringApplication.run(SpringbootwebApplication.class, args);
 		
 		
-		displayAllBeans();
+		//displayAllBeans();
 	}
 
 	public static void displayAllBeans() {
@@ -24,9 +36,32 @@ public class SpringbootwebApplication {
 		System.out.println("====================================================================");
 		
 		for (String beanName : allBeanNames) {
-			System.out.println(beanName);
+			System.out.println(beanName+ " : " + applicationContext.getBean(beanName).getClass().toString());
 		}
 		
 		System.out.println("====================================================================");
 	}
+	
+	// Register Servlet
+	  @Bean
+	  public ServletRegistrationBean servletRegistrationBean() {
+	    ServletRegistrationBean bean = new ServletRegistrationBean(
+	        new MyServlet(), "/myServlet");
+	    return bean;
+	  }
+
+	  // Register Filter
+	  @Bean
+	  public FilterRegistrationBean filterRegistrationBean() {
+	    FilterRegistrationBean bean = new FilterRegistrationBean(new MyFilter());
+	    // Mapping filter to a Servlet
+	    bean.addServletRegistrationBeans(new ServletRegistrationBean[] {
+	          servletRegistrationBean() 
+	       });
+	    return bean;
+	  }
+
+	  // Register ServletContextListener
+	  // Listener 들은 @WebListener 어노테이션을 이용해서 등록해야 한다.
+	  
 }
